054ec254cabb9e1d2a26a00a5b37374466617ee9
Dete110910 <alexanderlml1997@gmail.com>
2023-10-30 04:39:22 +0000

    Finish working_with_int.asm program

--- paul_carter_asm_io/working_with_int.asm
+++ paul_carter_asm_io/working_with_int.asm
@@ -1,85 +1,97 @@
 %include "asm_io.inc"
 
-%macro print_storage 2
-	pusha
-	mov eax, %1
-	call print_string
-	call read_int
-	mov ebx, %2
-	mov [ebx], eax
-	popa
+%macro print_storage 2					; Defines print_storage macro. It receives 2 parameters
+	pusha						; Saves the registers
+	mov eax, %1					; Store the first argument in eax register
+	call print_string				; Call the print_string function that prints the string that begins in address location in eax
+	call read_int					; Call the read_int function that reads the integer value typed by the user. It stores the value in eax
+	mov ebx, %2					; Moves the second argument to ebx register
+	mov [ebx], eax					; Moves the eax value to the address stored in ebx register. It stores eax value in the memory location pointed to ebx
+	popa						; Restore the register 
 %endmacro
 
-%macro square_number 2
-	pusha
-	mov ebx, %1
-	mov eax, [ebx]
-	mul eax
-	mov ebx, %2
-	mov [ebx], eax
-	popa
+%macro square_number 2					; Defines square_number macro. It receives 2 parameters
+	pusha						; Saves the registers
+	mov ebx, %1					; Stores the first argument in ebx register
+	mov eax, [ebx]					; Load the value stored in ebx address in eax
+	mul eax						; Multiplies the value of eax  by itself and stores in eax
+	mov ebx, %2					; Stores the second argument in ebx register
+	mov [ebx], eax					; Moves the eax value to the address stored in ebx register. It stores eax value in the memory location pointed to ebx
+	popa						; Restore registers
 %endmacro
 
-%macro multiply_storage 3
-	pusha
-	mov ebx, %1
-	mov eax, [ebx]
-	mov ecx, %2
-	mov edx, [ecx]
-	mul edx
-	mov ebx, %3
-	mov [ebx], eax
-	popa
+%macro multiply_storage 3				; Defines multiply_storage macro. It receives 3 parameters
+	pusha						; Saves the registers
+	mov ebx, %1					; Saves the first argument in ebx register
+	mov eax, [ebx]					; Saves the value stored in ebx memory addres in eax 
+	mov ecx, %2					; Saves the second parameter in ecx 
+	mov edx, [ecx]					; Saves the value of ecx in edx
+	mul edx						; Multiplies eax value to edx value and stores it in eax
+	mov ebx, %3					; Saves the third parameter in ebx 
+	mov [ebx], eax					; Moves the eax value to the address stored in ebx register. It stores eax value in the memory location pointed to ebx
+	popa						; Restore registers
 %endmacro
 
-%macro multiply_storage_const 3
-	pusha
-	mov ebx, %1
-	mov eax, [ebx]
-	mov ecx, %2
-	mul ecx
-	mov edx, %3
-	mov [edx], eax
-	popa
+%macro multiply_storage_const 3				; Defines multiply_storage_const macro. It receives 3 parameters
+	pusha						; Saves the registers
+	mov ebx, %1					; Store the first parameter in ebx 
+	mov eax, [ebx]					; Saves the ebx value in eax 
+	mov ecx, %2					; Store the second parameter in ecx 
+	mul ecx						; Multiplies eax value by ecx value and stores it in eax 
+	mov edx, %3					; Store the third parameter in edx
+	mov [edx], eax					; Moves the eax value to the address stored in ebx register. It stores eax value in the memory location pointed to edx
+	popa						; Restore registers
 %endmacro
 
-%macro division_storage_const 0 
-	pusha
-	
-	popa
+%macro division_storage_const 3				; Defines division_storage_const macro. It receives 3 parameters 
+	pusha						; Saves the registers
+	mov ebx, %1					; Store the first parameter in ebx
+	mov eax, [ebx]					; Saves the ebx value in eax
+	cdq 						; This instruction is used to convert dword (32 bits) to qword (64 bits). Later, u can perform signed division operation
+	mov ecx, 100					; Stores the 100 value to ecx 
+	idiv ecx					; Integer division. It divides eax value to ecx value. The quotient is stored in eax, the remainder in edx
+	mov ebx, %2					; Store the second parameter in ebx register
+	mov [ebx], eax					; Moves eax value to address located in ebx 
+	mov ebx, %3					; Store the third parameter in ebx
+	mov [ebx], edx					; Store edx value in ebx address location
+	popa						; Restore register
 %endmacro
 
-%macro print_result_with_nl 2
-	pusha
-	mov eax, %1
-	call print_string
-	mov ebx, %2
-	mov eax, [ebx]
-	call print_int
-	call print_nl
-	popa
+%macro negate_storage 2					; Defines negate_storage macro. It receives 2 parameters
+	pusha						; Saves the registers
+	mov ebx, %1					; Store the first parameter in ebx
+	mov eax, [ebx]					; Saves the ebx value in eax
+	neg eax						; Negate the eax value and stores in eax
+	mov ebx, %2					; Store the second parameter in ebx
+	mov [ebx], eax					; Saves the eax value in ebx address location
+	popa						; Restore registers
 %endmacro
 
-%macro print_data_message 2
-	pusha
-	mov eax, $1
-	call print_string
-	mov eax, $2
-	call print_int
-	popa
+%macro print_result_with_nl 2				; Defines print_result_with_nl macro. It receives 2 parameters
+	pusha						; Saves the registers
+	mov eax, %1					; Store the first parameter in eax
+	call print_string				; Call print_string function that prints the string which initial address are stored in eax
+	mov ebx, %2					; Store the second parameter in ebx
+	mov eax, [ebx]					; Saves the ebx value in eax
+	call print_int					; Call print_int function that prints value stored in eax
+	call print_nl					; Call print_nl function that prints new line
+	popa						; Restore registers
 %endmacro
+
 
 segment .data
 ;
 ; initialized data is put in the data segment here
 ;
 prompt1 db    "Enter a number: ", 0      
+prompt2 db    "Enter 1 to continue. Otherwise the program will close", 0
 outmsg1 db    "Square of input is: ", 0
 outmsg2 db    "Cube of input is: ", 0
 outmsg3 db    "Cube of input times 25 is: ", 0
 outmsg4 db    "Quotient of cube/100 is: ", 0
 outmsg5 db    "Remainder of cube/100 is: ", 0
 outmsg6 db    "The negation of remainder is: ", 0
+
 
 twenty_five equ 25
 one_hundred dd 100.0
@@ -91,50 +103,69 @@ segment .bss
 ;
 
 input1 resd 1
+input2 resd 1
 result resd 1
 square resd 1
 cube resd 2
 cube_times_tf resd 2
-quotient_cube_div_by_100 resd 2
+quotient_div_100 resd 2
 remainder resd 2
+neg_remainder resd 2
 
 segment .text
         global  asm_main
 asm_main:
         enter   0,0               ; setup routine
         pusha
-	
-	print_storage prompt1, input1
 
-	square_number input1, square
-
-	print_result_with_nl outmsg1, square
+start:
+	print_storage prompt1, input1					; Call macro that prints prompt1 message and store the entered value in input1 variable
 	
-	multiply_storage square, input1, cube
-	
-	print_result_with_nl outmsg2, cube
-	
-	multiply_storage_const cube, twenty_five, cube_times_tf
-	
-	print_result_with_nl outmsg3, cube_times_tf
+	square_number input1, square					; Call macro that squares the value of input1 variable and stores in the variable square
 
-	mov eax, [cube]
-	cdq
-	mov ecx, 100
-	idiv ecx
-	call print_nl
-	call print_int
+	print_result_with_nl outmsg1, square				; Call macro that print one message and the result. In this case, outmsg1 and square result
+	
+	multiply_storage square, input1, cube				; Call macro that multiply two numbers and stores it in one variable. In this case, multiply square with input1 and stores in cube
+	
+	print_result_with_nl outmsg2, cube				; Call macro that print one message and the result. In this case, outmsg2 and cube result
+	
+	multiply_storage_const cube, twenty_five, cube_times_tf		; Call macro that multiply two numbers and stores it in one variable. In this case, multiply cube  with twenty_five and stores in cube_times_tf
+	
+	print_result_with_nl outmsg3, cube_times_tf			; Call macro that print one message and the result. In this case, outmsg3 and cube_times_tf result
 
-	mov eax, edx
-	call print_nl
-	call print_int
+	division_storage_const cube, quotient_div_100, remainder	; Call macro that divide two numbers and storage the quotient and remainder in variables
 
-	mov ecx, eax
-	mov eax, outmsg4
-	call print_string
-	mov eax, ecx
-	call print_int
-	call print_nl
+	print_result_with_nl outmsg4, quotient_div_100			; Call macro that print one message and the result. In this case, outmsg4 and quotient_div_100 result
+
+	print_result_with_nl outmsg5, remainder				; Call macro that print one message and the result. In this case, outmsg5 and remainder result
+
+	negate_storage remainder, neg_remainder				; Call macro that negate and storage a number. In this case, negate remainder
+
+	print_result_with_nl outmsg6, neg_remainder			; Call macro that print one message and the result. In this case, outmsg6 and neg_remainder result
+
+	print_storage prompt2, input2					; Call macro that prints prompt2 message and store the entered value in input2 variable
+	mov eax, [input2]						; Moves the value of variable 2 in eax register
+	cmp eax, 1							; Compare the eax value with 1. It set the condition code in the machine status word
+	je start							; Conditional jump that are based on the status of a set of condition codes stores in a special register called machine status word. If the cmp operation performed is true, jump to start label
+	jne end								; Jump to end label if the last cmp operation performed is not equal
+
+;	mov eax, [cube]
+;	cdq
+;	mov ecx, 100
+;	idiv ecx
+;	call print_nl
+;	call print_int
+
+;	mov eax, edx
+;	call print_nl
+;	call print_int
+
+;	mov ecx, eax
+;	mov eax, outmsg4
+;	call print_string
+;	mov eax, ecx
+;	call print_int
+;	call print_nl
 
 ;	fld dword [cube]
 ;	fdiv dword [one_hundred]
@@ -155,6 +186,7 @@ asm_main:
 
 ;	print_result_with_nl outmsg4, quotient_cube_div_by_100 
 
+end:
         popa
         mov     eax, 0            ; return back to C
         leave                     
